ff2a9b0c5845dda35cc5c5a73b5da002338c6953
usayuki <masapp.01a@gmail.com>
2019-03-04 13:44:35 +0900

    initial commit

---
+++ AVCaptureSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AVCaptureSample
+//
+//  Created by usayuki on 2019/03/04.
+//  Copyright © 2019 usayuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
